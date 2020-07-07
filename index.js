b343d03798ce1494136eb37782da4d3c87dec862
hpc <philipchanhk626@gmail.com>
2020-07-07 15:23:56 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express')
-const app = express()
+const grabity = require('grabity')
+const bodyParser = require("body-parser");
+const router = express.Router();
+const app = express();
+
 let port = process.env.PORT || 4000
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
+app.use("/", router);
+
+router.get('/', (req, res) => {
   res.send('Hello')
 })
+
+async function getMetaData(url) {
+  if (url) {
+    const metaData = await grabity.grabIt(url)
+    return metaData
+  } else {
+    return
+  }
+}
+
+router.post('/', async (req, res) => {
+  const url = req.body.url
+  const meta = await getMetaData(url)
+  res.send(meta)
+})
+
 
 app.listen(port, () => {
   console.log('server is listening')
